32b51787b7c8386694c3d15bd5ab5c33abb0f772
hodasalimi <hoda.salimi@yahoo.com>
2021-04-15 19:32:13 +0200

    added new lines

--- src/components/home/sale.js
+++ src/components/home/sale.js
@@ -1,42 +1,55 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import {Link , NAVLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {URL_SALES} from '../utils/pathes';
 
+
+
 class Sale extends Component {
+    
     state = {
+        salesArray : []
+    }
 
-    }
-    fetchSales(){
-        
-            axios.get(URL_SALES)
-            .then(response =>{
-                this.setState({
-                    sales: response.data
-                })
-            })    
-    }
+
     componentDidMount(){
-        
+        axios.get(URL_SALES)
+            .then(response =>{
+                   this.setState({
+                        salesArray: response.data
+                   }) 
+            })
     }
+    
 
+    
     render() {
+        console.log(this.state)
         return (
             <>
-            <div className="container col-sm-12 text-center my-2 display-4">
-                Sales
-            </div>
-            <div className="sale-container col-sm-12 my-3 p-2">
-                {this.state.sales ?
-            <div className="sale-container d-flex">
-                {this.state.sales.map(
-                    <Link className="sale-item">
+                <div className="container col-sm-12 text-center my-2 display-4">
+                    Sales
+                </div>
+                <div className="container m-auto text-center col-sm-12 my-3 p-2">
+                   { 
+                        this.state.salesArray ?
+                            <div className="sale-container  d-flex flex-row justify-content-around flex-wrap align-content-center">
+                                {
+                                    this.state.salesArray.map( item =>(
+                                       
+                                        <Link to="" className="sale-item" key={item.saleId} style={{background :`url(/images/sales/${item.cover}) no-repeat `}}>
+                                            <div className="item-percent">10</div>
+                                        </Link>
+                                       
+                                       
+                                    )
+                                )}
+                            </div>
+                        : 
+                        null
+                    }
 
-                    </Link>
-                )}
-            </div>    
-            : null}
-            </div>
+                </div>
             </>
         )
     }
