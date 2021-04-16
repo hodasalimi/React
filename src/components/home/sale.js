d08d3a859f64c2bb9c3824cf6d6dcc859af9bb48
hodasalimi <hoda.salimi@yahoo.com>
2021-04-16 18:47:26 +0200

    added new lines to sale.js

--- src/components/home/sale.js
+++ src/components/home/sale.js
@@ -8,50 +8,317 @@ import {URL_SALES} from '../utils/pathes';
 class Sale extends Component {
     
     state = {
-        salesArray : []
+        salesArray : [],
+        firstSale:{
+            object : {},
+            active:false,
+            
+        },
+        secondSale:{
+            object : {},
+            active:false,
+           
+        },
+        thirdSale:{
+            object : {},
+            active:false,
+            
+        },
+        fourthSale:{
+            object : {},
+            active:false,
+            
+        }
     }
 
-
-    componentDidMount(){
-        axios.get(URL_SALES)
-            .then(response =>{
-                   this.setState({
-                        salesArray: response.data
-                   }) 
-            })
-    }
     
-
+        handleMouseOver = (event,saleUrl)=>{
+            
+            switch(saleUrl){
+                case 1:{
+                    if(event.target.className === 'item-percent' && !this.state.firstSale.active){
+                        this.state.firstSale.active = true;
+                        const percent = Number(this.state.firstSale.object.percent);
+                        let start =0;
+                        const timer = setInterval(()=>{
+                            event.target.innerHTML = `<span class="mr-3">SALE</span>  ${start}%`;
+                            if(start === percent){
+                                clearInterval(timer);
+                            }else{
+                                start++;
+                            }
+                        },15);
+                    }
+                    if(!this.state.firstSale.active){ 
+                    this.state.firstSale.active = true;
+                    const percent = Number(this.state.firstSale.object.percent);
+                    let start =0;
+                    if(event.target.className === 'sale-item'){
+                            const timer = setInterval(()=>{
+                                event.target.querySelector('div').innerHTML = `<span class="mr-3">SALE</span>  ${start}%`;
+                                if(start === percent){
+                                    clearInterval(timer);
+                                }else{
+                                    start++;
+                                }
+                            },15);
+                        }else if(event.target.className === 'item-percent' && this.state.firstSale.active){
+    
+                        }
+                    }else if(this.state.firstSale.active){
+    
+   
+    } 
+                   
+                        break;
+                    }
+                    case 2:{
+                        if(event.target.className === 'item-percent' && !this.state.secondSale.active){
+                            this.state.secondSale.active = true;
+                            const percent = Number(this.state.secondSale.object.percent);
+                            let start =0;
+                            const timer = setInterval(()=>{
+                                event.target.innerHTML = `<span class="mr-3">SALE</span>  ${start}%`;
+                                if(start === percent){
+                                    clearInterval(timer);
+                                }else{
+                                    start++;
+                                }
+                            },15);
+                        }
+                        if(!this.state.secondSale.active){ 
+                        this.state.secondSale.active = true;
+                        const percent = Number(this.state.secondSale.object.percent);
+                        let start =0;
+                        if(event.target.className === 'sale-item'){
+                                const timer = setInterval(()=>{
+                                    event.target.querySelector('div').innerHTML = `<span class="mr-3">SALE</span>  ${start}%`;
+                                    if(start === percent){
+                                        clearInterval(timer);
+                                    }else{
+                                        start++;
+                                    }
+                                },15);
+                            }else if(event.target.className === 'item-percent' && this.state.secondSale.active){
+        
+                            }
+                        }else if(this.state.secondSale.active){
+        
+                        } 
+                       
+                            break;
+                        }
+                        case 3:{
+                            if(event.target.className === 'item-percent' && !this.state.thirdSale.active){
+                                this.state.thirdSale.active = true;
+                                const percent = Number(this.state.thirdSale.object.percent);
+                                let start =0;
+                                const timer = setInterval(()=>{
+                                    event.target.innerHTML = `<span class="mr-3">SALE</span>  ${start}%`;
+                                    if(start === percent){
+                                        clearInterval(timer);
+                                    }else{
+                                        start++;
+                                    }
+                                },15);
+                            }
+                            if(!this.state.thirdSale.active){ 
+                            this.state.thirdSale.active = true;
+                            const percent = Number(this.state.thirdSale.object.percent);
+                            let start =0;
+                            if(event.target.className === 'sale-item'){
+                                    const timer = setInterval(()=>{
+                                        event.target.querySelector('div').innerHTML =` <span class="mr-3">SALE</span>  ${start}%`;
+                                        if(start === percent){
+                                            clearInterval(timer);
+                                        }else{
+                                            start++;
+                                        }
+                                    },15);
+                                }else if(event.target.className === 'item-percent' && this.state.thirdSale.active){
+            
+                                }
+                            }else if(this.state.thirdSale.active){
+            
+                            } 
+                           
+                                break;
+                            }
+                            case 4:{
+                                if(event.target.className === 'item-percent' && !this.state.fourthSale.active){
+                                    this.state.fourthSale.active = true;
+    
+    
+    const percent = Number(this.state.fourthSale.object.percent);
+                                    let start =0;
+                                    const timer = setInterval(()=>{
+                                        event.target.innerHTML = `<span class="mr-3">SALE</span>  ${start}%`;
+                                        if(start === percent){
+                                            clearInterval(timer);
+                                        }else{
+                                            start++;
+                                        }
+                                    },15);
+                                }
+                                if(!this.state.fourthSale.active){ 
+                                this.state.fourthSale.active = true;
+                                const percent = Number(this.state.fourthSale.object.percent);
+                                let start =0;
+                                if(event.target.className === 'sale-item'){
+                                        const timer = setInterval(()=>{
+                                            event.target.querySelector('div').innerHTML = `<span class="mr-3">SALE</span>  ${start}%`;
+                                            if(start === percent){
+                                                clearInterval(timer);
+                                            }else{
+                                                start++;
+                                            }
+                                        },15);
+                                    }else if(event.target.className === 'item-percent' && this.state.fourthSale.active){
+                
+                                    }
+                                }else if(this.state.fourthSale.active){
+                
+                                } 
+                               
+                                    break;
+                                }
+                                default:{
+                                    console.log(saleUrl);
+                                }
+    
+            }
+        }
+    
+        handleMouseLeave = (event,id) => {
+            switch(id){
+                case 1:{
+                    if(this.state.firstSale.active){ 
+                        this.state.firstSale.active = false;
+                        if(event.target.className === 'sale-item'){
+                            event.target.querySelector('div').textContent = `${this.state.firstSale.object.topic}`;       
+                            }   
+                            if(event.target.className === 'item-percent' && !this.state.firstSale.active){
+                                    event.target.textContent = `${this.state.firstSale.object.topic}`;
+                                
+                            }else if(event.target.className === 'item-percent' && this.state.firstSale.active){
+    
+                            }
+                        }else if(!this.state.firstSale.active){
+    
+    
+    }
+                        break;
+                    }
+                    case 2:{
+                        if(this.state.secondSale.active){ 
+                            this.state.secondSale.active = false;
+                            if(event.target.className === 'sale-item'){
+                                event.target.querySelector('div').textContent = `${this.state.secondSale.object.topic}`;       
+                                }   
+                                if(event.target.className === 'item-percent' && !this.state.secondSale.active){
+            
+                                        event.target.textContent = `${this.state.secondSale.object.topic}`;
+                                    
+                                }else if(event.target.className === 'item-percent' && this.state.secondSale.active){
+        
+                                }
+                            }else if(!this.state.secondSale.active){
+        
+                            }
+                            break;
+                        }
+                        case 3:{
+                            if(this.state.thirdSale.active){ 
+                                this.state.thirdSale.active = false;
+                                if(event.target.className === 'sale-item'){
+                                    event.target.querySelector('div').textContent = `${this.state.thirdSale.object.topic}`;       
+                                    }   
+                                    if(event.target.className === 'item-percent' && !this.state.thirdSale.active){
+                        
+                                            event.target.textContent = `${this.state.thirdSale.object.topic}`;
+                                        
+                                    }else if(event.target.className === 'item-percent' && this.state.thirdSale.active){
+            
+                                    }
+                                }else if(!this.state.thirdSale.active){
+            
+                                }
+                                break;
+                            }
+                            case 4:{
+                                if(this.state.fourthSale.active){ 
+                                    this.state.fourthSale.active = false;
+                                    if(event.target.className === 'sale-item'){
+                                        event.target.querySelector('div').textContent = `${this.state.fourthSale.object.topic}`;       
+                                        }   
+                                        if(event.target.className === 'item-percent' && !this.state.fourthSale.active){
+                                                
+                                                event.target.textContent = `${this.state.fourthSale.object.topic}`;
+                                            
+                                        }else if(event.target.className === 'item-percent' && this.state.fourthSale.active){
+                
+                                        }
+                                    }else if(!this.state.fourthSale.active){
+                                        
+                                    }
+                                    break;
+                                }
+                                default:{
+                                    console.log(id);
+                                }
+    
+            }
+        }
+        
+        componentDidMount(){
+            axios.get(URL_SALES)
+                .then(response =>{
+                       this.setState({
+                            salesArray: response.data
+                       })
+                       this.state.firstSale.object = this.state.salesArray[0]; 
+                       this.state.secondSale.object = this.state.salesArray[1];
+                       this.state.thirdSale.object = this.state.salesArray[2];
+                       this.state.fourthSale.object = this.state.salesArray[3];
+                })
+        }
+    
     
     render() {
-        console.log(this.state)
-        return (
-            <>
-                <div className="container col-sm-12 text-center my-2 display-4">
-                    Sales
-                </div>
-                <div className="container m-auto text-center col-sm-12 my-3 p-2">
-                   { 
-                        this.state.salesArray ?
-                            <div className="sale-container  d-flex flex-row justify-content-around flex-wrap align-content-center">
-                                {
-                                    this.state.salesArray.map( item =>(
+            console.log(this.state)
+            return (
+                <>
+                    <div className="container col-sm-12 text-center my-2 display-4">
+                        Sales
+                    </div>
+                    <div className="container m-auto text-center col-sm-12 my-3 p-2">
+                       { 
+                            this.state.salesArray ?
+                                <div className="sale-container  d-flex flex-row justify-content-around flex-wrap align-content-center">
+                                    {
+                                        this.state.salesArray.map( item =>(
+                                            
+                                            <Link 
+                                            to="" 
+                                            className="sale-item" 
+                                            key={item.id} 
+                                            style={{background :`url(/images/sales/${item.cover}) no-repeat `}}
+                                            onMouseOver={(event,saleUrl)=>this.handleMouseOver(event,item.id)}
+                                            onMouseLeave={(event,saleUrl)=>this.handleMouseLeave(event,item.id)}
+                                            > 
+                                                <div className="item-percent">{item.topic}</div> 
+                                            </Link>     
                                        
-                                        <Link to="" className="sale-item" key={item.saleId} style={{background :`url(/images/sales/${item.cover}) no-repeat `}}>
-                                            <div className="item-percent">10</div>
-                                        </Link>
-                                       
-                                       
-                                    )
-                                )}
-                            </div>
-                        : 
-                        null
-                    }
-
-                </div>
-            </>
-        )
+                                        )
+                                    )}
+                                </div>
+                            : 
+                            null
+                        }
+    
+                    </div>
+                </>
+            )
+        }
     }
-}
-export default Sale;
+    export default Sale;
